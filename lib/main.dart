fc372a36283c9d2c380c5286443ca4fd2519ba72
Mohammed <mohammedemad557@yahoo.com>
2026-09-16 20:36:30 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const Test2(title: 'ephemeral state'),
+    );
+  }
+}
+
+/// imperative UI -==> as Native Swift do
+/// Declarative UI ==> as Flutter DO
+
+// How flutter handle state
+
+//provider
+
+//immutable  => غير قابل للتغير
+class Test extends StatelessWidget {
+  const Test({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [TextButton(onPressed: () {}, child: Text("press here"))],
+      ),
+    );
+  }
+}
+
+///       ephemeral state ===> حالة سريعة الزوال
+//immutable  => غير قابل للتغير
+class Test2 extends StatefulWidget {
+  const Test2({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<Test2> createState() => _Test2State();
+}
+
+class _Test2State extends State<Test2> {
+  List<String> names = ["Ahmed Hany", "Mena Adel"];
+  TextEditingController name = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text(
+          widget.title,
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w900,
+            fontSize: 24,
+            letterSpacing: 2,
+          ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                padding: EdgeInsets.symmetric(horizontal: 24, vertical: 31),
+                itemCount: names.length,
+                itemBuilder: (context, index) => Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 5.0),
+                  child: Text(
+                    "● ${names[index]}",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.indigo,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // showDialog(
+          //   context: context,
+          //   builder: (context) => AlertDialog(
+          //     title: Text("Enter New Name"),
+          //     content: TextFormField(controller: name),
+          //     actions: [
+          //       ElevatedButton(
+          //         onPressed: () {
+          //           Navigator.pop(context, name.text);
+          //         },
+          //         child: Text("add"),
+          //       ),
+          //     ],
+          //   ),
+          // ).then((value) {
+          //   names.add(value);
+          //   setState(() {});
+          // });
+
+
+        },
+        child: Text(
+          "+",
+          style: TextStyle(color: Colors.indigo.shade400, fontSize: 29),
+        ),
+      ),
+    );
+  }
+}
